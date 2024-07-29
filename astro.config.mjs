89ce90a38385a179d1943857de0827b16bcc80e9
Thomas Bonnet <thomasbnt@protonmail.com>
2024-07-30 01:51:11 +0200

    modify Header and PageSidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,6 +30,7 @@ export default defineConfig({
       ],
       components: {
         Header: "./src/components/Header.astro",
+        PageSidebar: "./src/components/PageSidebar.astro",
       },
       head: [
         {
